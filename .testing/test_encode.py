a5f6b2aa8b31f4434f653cd2be9d6f5f57e2a4e1
Cersei <bestkarmarussia@gmail.com>
2025-03-18 02:19:28 +0300

    test done

--- .testing/test_encode.py
+++ .testing/test_encode.py
@@ -4,11 +4,9 @@ def encode_image_to_base64(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode("utf-8")
 
-# Укажи путь к изображению
 image_base64 = encode_image_to_base64(".testing/test_image.png")
 
-# Сохраняем base64 в файл (для удобства копирования)
 with open(".testing/image_base64.txt", "w") as f:
     f.write(image_base64)
 
-print("✅ Base64-код сохранён в image_base64.txt")
+print("DONE")
